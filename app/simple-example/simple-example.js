c73d4d5d7d8570f8118085ad71b53083eb25d7f3
Christopher Baines <cb15g11@soton.ac.uk>
2014-10-03 12:48:07 +0100

    Use Big Buck Bunny

--- app/simple-example/simple-example.js
+++ app/simple-example/simple-example.js
@@ -12,9 +12,7 @@ angular.module('videogularQuizExample.simpleExample', ['ngRoute'])
 .controller('SimpleExampleCtrl', function($scope, $sce) {
 	$scope.config = {
 		sources: [
-			{src: $sce.trustAsResourceUrl("http://www.videogular.com/assets/videos/videogular.mp4"), type: "video/mp4"},
-			{src: $sce.trustAsResourceUrl("http://www.videogular.com/assets/videos/videogular.webm"), type: "video/webm"},
-			{src: $sce.trustAsResourceUrl("http://www.videogular.com/assets/videos/videogular.ogg"), type: "video/ogg"}
+			{src: $sce.trustAsResourceUrl("video.ogg"), type: "video/ogg"}
 		],
 		theme: {
 			url: "simple-example/videogular.css"
