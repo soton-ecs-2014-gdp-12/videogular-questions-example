5dfef73d9a81ed8b90cd31f9b2cba7b11b257dbc
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-07 13:42:37 +0100

    Added new download file to download all types of file format

---
+++ downloadVideo.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget -nc -O app/video.ogg http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_stereo.ogg
+wget -nc -O app/video.mov http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov
+wget -nc -O app/video.avi http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_stereo.avi
+wget -nc -O app/video.mp4 http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4
+wget -nc -O app/video.m4v http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v
+
+echo "Fake echo to ensure return code is 0, check wget output to ensure valid behavior"
