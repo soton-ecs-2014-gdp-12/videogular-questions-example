f9b8359e3cce614e485e0be0a9bd32d644e6a200
Samuel Bennett <sb21g11@soton.ac.uk>
2014-10-08 10:46:21 +0100

    Fix broken linking in script

--- scripts/startdev.sh
+++ scripts/startdev.sh
@@ -2,6 +2,6 @@ rm -rf app/bower_components/videogular-questions
 ln -s ../../../videogular-questions app/bower_components/videogular-questions
 
 rm -rf app/bower_components/videogular-controls
-ln -s ../../../videogular-controls app/bower_components/videogular-controls
+ln -s ../../../bower-videogular-controls app/bower_components/videogular-controls
 
 http-server -a localhost -p 8000 -c-1
