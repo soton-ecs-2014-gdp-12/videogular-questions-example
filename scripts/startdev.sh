81c02cb41a2658d76b992d128d90cb03485891de
Christopher Baines <cb15g11@soton.ac.uk>
2014-10-15 15:11:35 +0100

    Don't assume that http-server is on the path

--- scripts/startdev.sh
+++ scripts/startdev.sh
@@ -4,4 +4,4 @@ ln -s ../../../videogular-questions app/bower_components/videogular-questions
 rm -rf app/bower_components/videogular-controls
 ln -s ../../../bower-videogular-controls app/bower_components/videogular-controls
 
-http-server -a localhost -p 8000 -c-1
+node_modules/http-server/bin/http-server -a localhost -p 8000 -c-1
