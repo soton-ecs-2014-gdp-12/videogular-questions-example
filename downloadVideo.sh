a0e3747570e98cdc626a9aef60e038fc5d95f718
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-21 11:21:26 +0000

    Changed download path

--- downloadVideo.sh
+++ downloadVideo.sh
@@ -6,6 +6,6 @@ wget -nc -O app/video.avi http://download.blender.org/peach/bigbuckbunny_movies/
 wget -nc -O app/video.mp4 http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4
 wget -nc -O app/video.m4v http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v
 
-wget -nc -O http://cbaines.net/caesar-cipher.mp4
+wget -nc -O app/caesar-cipher.mp4 http://cbaines.net/caesar-cipher.mp4
 
 echo "Fake echo to ensure return code is 0, check wget output to ensure valid behavior"
