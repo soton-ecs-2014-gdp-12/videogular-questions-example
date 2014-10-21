9a84c3cd898dff9e9bf6ec02b1e76007a6ac5515
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-21 12:06:22 +0000

    Changed startdev so it doesnt link to controls

--- scripts/startdev.sh
+++ scripts/startdev.sh
@@ -1,7 +1,7 @@
 rm -rf app/bower_components/videogular-questions
 ln -s ../../../videogular-questions app/bower_components/videogular-questions
 
-rm -rf app/bower_components/videogular-controls
-ln -s ../../../bower-videogular-controls app/bower_components/videogular-controls
+#rm -rf app/bower_components/videogular-controls
+#ln -s ../../../bower-videogular-controls app/bower_components/videogular-controls
 
 node_modules/http-server/bin/http-server -a localhost -p 8000 -c-1
