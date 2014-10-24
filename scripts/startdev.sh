c26215ee29998a45e059657582ef647e8dad9289
Christopher Hewett <chewett@hotmail.co.uk>
2014-10-24 13:57:22 +0000

    Added comment to devscript

--- scripts/startdev.sh
+++ scripts/startdev.sh
@@ -4,6 +4,7 @@ ln -s ../../../videogular-questions app/bower_components/videogular-questions
 rm -rf app/bower_components/videogular-cuepoints
 ln -s ../../../videogular-cuepoints app/bower_components/videogular-cuepoints
 
+#Commented out since we might want to use this later
 #rm -rf app/bower_components/videogular-controls
 #ln -s ../../../bower-videogular-controls app/bower_components/videogular-controls
 
